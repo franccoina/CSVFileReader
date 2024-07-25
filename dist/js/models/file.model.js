503413a8abd0b7741fae4834d0fb8ecf5ae54e04
franccoina <franccoina@gmail.com>
2024-07-25 09:20:10 -0500

    feat:Adding controllers and models for processing uploaded file

---
+++ dist/js/models/file.model.js
@@ -0,0 +1,7 @@
+//----------------------------------- Interfaces -----------------------------------
+export {};
+//----------------------------------- Data Schema -----------------------------------
+//For better understanding, here you have a schema of how we are structuring the file data 
+//[{ 'Departamento': 'Antioquia', 'Ciudad': 'Medellín' }, { 'Departamento': 'Cundinamarca', 'Ciudad': 'Bogotá' }]
+//WHERE: [] Table, is an Array with the table rows
+//       {} Table Rows, is an Object with each value, related to a Column Name with identifies the value
