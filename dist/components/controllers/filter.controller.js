bf2869cc92fc9e7e69802274ca460fda82ec4d47
franccoina <franccoina@gmail.com>
2024-07-26 13:28:36 -0500

    fix:Adding functions to index.ts and setting interactivity... in process

--- dist/components/controllers/filter.controller.js
+++ dist/components/controllers/filter.controller.js
@@ -1,17 +1,13 @@
 //---------------------- Function for filtering records or rows from our Table -------------------
 export function filterData(tableArray, searchTerm) {
     //If the searcher is empty
-    if (searchTerm === undefined) {
+    if (!searchTerm) {
         return tableArray; //Error handler: No need to filter, so return the original array
     }
     const lowerCaseSearchTerm = searchTerm.toLowerCase();
-    const resultFilterData = tableArray.filter(row => Object.values(row).some(cell => {
-        if (cell === null || cell === undefined) {
+    return tableArray.filter(row => Object.values(row).some(cell => {
+        if (cell === null || cell === undefined)
             return false; //Error handler. We can't use and empty or undefined cell
-        }
-        else {
-            return cell.toString().toLowerCase().includes(lowerCaseSearchTerm);
-        }
+        return cell.toString().toLowerCase().includes(lowerCaseSearchTerm);
     }));
-    return resultFilterData;
 }
